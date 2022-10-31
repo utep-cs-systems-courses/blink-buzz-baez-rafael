ecdc26309a6342472058608be478f395e038f7f5
baez-rafael <baezrafael03@gmail.com>
2022-10-30 20:43:34 -0600

    Reverted demo 6

--- demos/6-led-assy/greenControl-s.s
+++ demos/6-led-assy/greenControl-s.s
@@ -1,13 +1,9 @@
 	.arch msp430g2553
 	.p2align 1,0
-	.data
-tempSub:	.word 0
-blinkLimit:	.long 5
-blinkCount:	.long 0
 	.text
 
+
 	.global greenControl
-	.global blinkUpdate
 	.extern P1OUT
 
 greenControl:
@@ -17,19 +13,3 @@ greenControl:
 	pop r0
 off:	and #~64, &P1OUT
 	pop r0
-	
-blinkUpdate:
-	add #1, blinkCount
-	mov #blinkCount, tempSub
-	sub blinkLimit, tempSub
-	jc bUpdateOff
-	mov #0, blinkCount
-	clrc
-	mov #1, r12
-	call greenControl
-	pop r0
-bUpdateOff:	clrc
-	mov #0, r12
-	call greenControl
-	pop r0
-	
